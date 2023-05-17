8af4bba7af38d1de2e16787f391d9fcf03695fd5
Alex_N <120394126+Sanya486@users.noreply.github.com>
2023-05-17 19:18:37 +0300

    task comleted

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export function fetchCountries(name) {
+  const params = new URLSearchParams({
+    fields: 'name,flags,capital,population,languages',
+  });
+  return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
+    countries => {
+      if (!countries.ok) {
+        throw new Error();
+      }
+      return countries.json();
+    }
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.inputEl.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
+
+function searchCountries(e) {
+  clearAllMarkUp();
+  if (e.target.value === "") {
+      return
+  }
+  else {
+    fetchCountries(e.target.value.trim())
+      .then(countries => {
+        if (countries.length <= 10 && countries.length > 1) {
+          countries.forEach(({flags, name}) =>
+            createListMarkUp(flags.png, name.official)
+          );
+        } else if (countries.length === 1) {
+          createCountryInfoPlate({
+            flaglink: countries[0].flags.png,
+            flagAlt: countries[0].flags.alt,
+            name: countries[0].name.official,
+            population: countries[0].population,
+            capital: countries[0].capital,
+            languages: Object.values(countries[0].languages),
+          });
+        } else if (countries.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        }
+      })
+      .catch(() => Notify.failure('Oops, there is no country with that name'));
+  }
+    
+}
+
+function createListMarkUp(flagLink, country) {
+    const markUp = `<li class="list-item">
+        <p><img src="${flagLink}" alt="" width = 30px>    ${country}</p>
+      </li>`;
+    refs.countryList.insertAdjacentHTML('beforeend', markUp);  
+}
+
+function createCountryInfoPlate({flaglink, flagAlt, name, population, capital, languages}) {
+  const markUp = `<div class="plate-wrap">
+    <h1><img src="${flaglink}" alt="${flagAlt}" width= 30px>  ${name}</h1>
+      <ul>
+        <li><span>Capital:</span> ${capital}</li>
+        <li><span>Population:</span> ${population}</li>
+        <li><span>Languages:</span> ${languages}</li>
+    </ul></div>`;
+    refs.countryInfo.innerHTML = markUp;
+}
+function clearAllMarkUp() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
+
+
+
